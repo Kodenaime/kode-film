c0c7a51b51e369f81bfb18bb747ea91b6345ee0f
iamkodenaime <creativedannyk@gmail.com>
2025-01-07 18:14:52 +0100

    finished with the navbar

--- film/src/components/Navbar/Navbar.jsx
+++ film/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './navbar.css'
 
+import logo from '../../assets/mainlogo.png'
+import searchi from '../../assets/search_icon.svg'
+import bell from '../../assets/bell_icon.svg'
+import profilei from '../../assets/profile_img.png'
+import caret from '../../assets/caret_icon.svg'
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+
+      <div className="nav-left">
+          <img src={logo} alt="" />
+          <ul>
+            <li>Home</li>
+            <li>Movies</li>
+            <li>TV Shows</li>
+            <li>New & Popular</li>
+            <li>My List</li>
+            <li>Browse</li>
+          </ul>
+      </div>
+
+      <div className="nav-right">
+        <img src={searchi} alt="search icon" className='icons' />
+        <p>Children</p>
+        <img src={bell} alt="bell icon" className='icons' />
+
+        <div className="nav-profile">
+            <img src={profilei} alt="profile image" className='profile' />
+            <img src={caret} alt="caret icon"  />
+
+            <div className="drop">
+              <p>Sign Out</p>
+            </div>
+
+        </div>
+      </div>
+
+    </div>
   )
 }
 
--- film/src/pages/Home/Home.jsx
+++ film/src/pages/Home/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+ import React from 'react'
 import './home.css'
+
+import Navbar from '../../components/Navbar/Navbar'
+import hero from '../../assets/hero_banner.jpg'
+import heroT from '../../assets/hero_title.png'
+import play from '../../assets/play_icon.png'
+import info from '../../assets/info_icon.png'
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <Navbar /> 
+      <div className="hero">
+        <img src={hero} alt="image banner" className='banner-img' />
+        <div className="hero-caption">
+          <img src={heroT} alt="hero title" className='caption' />
+          <p>discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+        </div>
+
+        <div className="hero-btns">
+          <button className='btn'>
+            <img src={play} alt="play movie icon" /> Play
+          </button>
+          <button className='btn dark'>
+            <img src={info} alt="play movie icon" /> Info
+          </button>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
