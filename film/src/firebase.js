480cb17d5347bad983a2fb5d78a02b82428a29f9
iamkodenaime <creativedannyk@gmail.com>
2025-01-15 20:49:37 +0100

    added the login and navigate functionality

---
+++ film/src/firebase.js
@@ -0,0 +1,50 @@
+
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signOut } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth/cordova";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCrYzpIGxYyDur141EL8sYqlJGIQ1JzNUM",
+  authDomain: "kode-movies.firebaseapp.com",
+  projectId: "kode-movies",
+  storageBucket: "kode-movies.firebasestorage.app",
+  messagingSenderId: "1039009179661",
+  appId: "1:1039009179661:web:8d2999a2c7e4438e0f4d44"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+const signup = async (name, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc(collection(db, "user"), {
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        });
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const login = async (email, password) => {
+    try {
+        signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const logout = () => {
+    signOut(auth);
+}
+
+export {auth, db, login, signup, logout};
--- film/src/pages/Login/Login.jsx
+++ film/src/pages/Login/Login.jsx
@@ -1,26 +1,41 @@
 import React from 'react'
 import './login.css'
 
+import { login, signup } from '../../firebase'
 import logo from '../../assets/mainlogo.png'
 import { useState } from 'react'
 
 const Login = () => {
 
-  const [login, setLogin] = useState("Sign In")
+  const [signState, setSignState] = useState("Sign In")
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState(""); 
+  const [password, setPassword] = useState("");
+
+  //user authentication function
+  const user_auth = async (event) => {
+    event.preventDefault();
+    if(signState === "Sign In") {
+      await login(email, password);
+    } else {
+      await signup(name, email, password);
+    }
+  } 
 
   return (
     <div className='login'>
       <img src={logo} alt="website logo" className='logos' />
       <div className="form">
-        <h1>{login}</h1>
+        <h1>{signState}</h1>
         <form >
           {
-            login === "Sign Up" ? <input type="text" placeholder='Full Name' /> : <></>
+            signState === "Sign Up" ? <input type="text" value={name} onChange={(e) => {setName(e.target.value)}} placeholder='Full Name' /> : <></>
           }
           
-          <input type="email" placeholder='Email'/>
-          <input type="password" placeholder='password' />
-          <button>{login}</button>
+          <input type="email" placeholder='Email' value={email} onChange={(e) => {setEmail(e.target.value)}} />
+          <input type="password" placeholder='password' value={password} onChange={(e) => {setPassword(e.target.value)}} />
+          <button onClick={user_auth} type='submit' >{signState}</button>
 
           <div className="help">
             <div className="remenber"><input type="checkbox" /> <label htmlFor="">Keep Me Signed In</label> </div>
@@ -30,9 +45,9 @@ const Login = () => {
         </form>
         <div className="switch">
           {
-            login === "Sign In" ?
-            <p>New To Kode Movies? <span onClick={() => {setLogin("Sign Up")}}>Sign Up Now</span></p> :
-            <p>Already Have an Accoubt? <span onClick={() => {setLogin("Sign In")}}>Sign In</span></p> 
+            signState === "Sign In" ?
+            <p>New To Kode Movies? <span onClick={() => {setSignState("Sign Up")}}>Sign Up Now</span></p> :
+            <p>Already Have an Accoubt? <span onClick={() => {setSignState("Sign In")}}>Sign In</span></p> 
           }
           
           
